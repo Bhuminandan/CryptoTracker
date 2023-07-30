a59017032f3b149777ea77713e51862542c1ddbe
Bhumi <bhumione9545@gmail.com>
2023-07-30 10:54:10 +0530

    Bug Fixes

--- script/script.js
+++ script/script.js
@@ -6,14 +6,15 @@ let mobThemeBtn = document.querySelector(".mobile-header__switch-btn>input")
 
 let tableTotalValume = document.querySelector(".table-total__valume");
 let tableTotalCap = document.querySelector(".table-market__cap");
-themeBtn.addEventListener("click", () => {
+
+themeBtn.addEventListener("change", () => {
     document.body.classList.toggle("dark-mode");
 
     tableTotalValume.style.color = "#6c6c6c";
     tableTotalCap.style.color = "#6c6c6c";
 })
 
-mobThemeBtn.addEventListener("click", () => {
+mobThemeBtn.addEventListener("change", () => {
     document.body.classList.toggle("dark-mode");
     tableTotalValume.style.color = "#fff";
     tableTotalCap.style.color = "#fff";
@@ -45,7 +46,7 @@ mainHeaderLeft.addEventListener("click", () => {
     mainHeaderLeft.classList.add("main__header-active");
     mainHeaderRight.classList.remove("main__header-active")
     main.innerHTML = "";
-    defaultPageGridData (); 
+    defaultPageGridData();
 })
 
 let table = document.getElementById("table");
@@ -56,10 +57,14 @@ mainHeaderRight.addEventListener("click", () => {
     mainHeaderRight.classList.add("main__header-active");
     mainHeaderLeft.classList.remove("main__header-active");
     async function getData() {
-        let url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en";
+        try {let url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en";
         let response = await fetch(url);
         let data = await response.json();
         createList(data);
+       } catch(error){
+          main.innerHTML = "";
+          table.innerHTML = `<h4>Failed to fetch Due to ${error}, please try after some time!</h4>`
+        }
     }
     getData()
 
@@ -67,7 +72,7 @@ mainHeaderRight.addEventListener("click", () => {
         dataList.forEach((singleData) => {
             let tr = document.createElement("tr");
             tr.classList.add("tr");
-            
+
             let trInnerHtml = `<td>
                                     <div class="table-main__grid-card-top">
                                     <div class="table-main__grid-card-top-left">
@@ -96,38 +101,50 @@ mainHeaderRight.addEventListener("click", () => {
                                 </td>`
             tr.innerHTML = trInnerHtml;
             // Get the elements inside the card
-        let percentageDiv = tr.querySelector(".table-percentage");
-        let priceDiv = tr.querySelector(".table-price");
+            let percentageDiv = tr.querySelector(".table-percentage");
+            let priceDiv = tr.querySelector(".table-price");
 
-        // Apply styles based on the condition
-        if (singleData.price_change_percentage_24h < 0) {
-            percentageDiv.style.borderColor = "#e7514a";
-            percentageDiv.style.color = "#e7514a";
-            priceDiv.style.color = "#e7514a";
-        }
+            
+            let volumeDiv = tr.querySelector(".table-total__valume");
+            let marketCapDiv = tr.querySelector(".table-market__cap");
+
+            volumeDiv.style.color = "#fff";
+            marketCapDiv.style.color = "#fff";
+
+            // Apply styles based on the condition
+            if (singleData.price_change_percentage_24h < 0) {
+                percentageDiv.style.borderColor = "#e7514a";
+                percentageDiv.style.color = "#e7514a";
+                priceDiv.style.color = "#e7514a";
+            }
             table.appendChild(tr);
         })
     }
 
 })
 
-function defaultPageGridData () {async function getData() {
-                let url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en";
-                let response = await fetch(url);
-                let data = await response.json();
-                createGrid(data);
-            }
+function defaultPageGridData() {
+    async function getData() {
+        try {let url = "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false&locale=en";
+        let response = await fetch(url);
+        let data = await response.json();
+        createGrid(data);
+       } catch(error){
+          main.innerHTML = "";
+          table.innerHTML = `<h4>Failed to fetch Due to ${error}, please try after some time!</h4>`
+        }
+    }
 
-            getData();
+    getData();
 
-            let mainGrid = document.querySelector(".main__grid");
+    let mainGrid = document.querySelector(".main__grid");
 
-            function createGrid(dataList) {
-                dataList.forEach((singleObj) => {
-                    
-                    let gridElement = document.createElement("div");
-                    gridElement.classList.add("main__grid-card");
-                    let innerHtmlCard = `<div class="main__grid-card-top">
+    function createGrid(dataList) {
+        dataList.forEach((singleObj) => {
+
+            let gridElement = document.createElement("div");
+            gridElement.classList.add("main__grid-card");
+            let innerHtmlCard = `<div class="main__grid-card-top">
                                                 <div class="main__grid-card-top-left">
                                                 <img src="${singleObj.image}" alt="icon imag" />
                                                 </div>
@@ -142,46 +159,46 @@ function defaultPageGridData () {async function getData() {
                                                 <div class="total__valume">Total Valume: ${singleObj.total_volume}</div>
                                                 <div class="market__cap">Market Cap:$ ${singleObj.market_cap}</div>
                                             </div>`
-                    gridElement.innerHTML = innerHtmlCard;
+            gridElement.innerHTML = innerHtmlCard;
 
-                    // Get the elements inside the card
-                    let percentageDiv = gridElement.querySelector(".percentage");
-                    let priceDiv = gridElement.querySelector(".price");
+            // Get the elements inside the card
+            let percentageDiv = gridElement.querySelector(".percentage");
+            let priceDiv = gridElement.querySelector(".price");
 
-                    // Apply styles based on the condition
-                    if (singleObj.price_change_percentage_24h < 0) {
-                        percentageDiv.style.borderColor = "#e7514a";
-                        percentageDiv.style.color = "#e7514a";
-                        priceDiv.style.color = "#e7514a";
-                    }
+            // Apply styles based on the condition
+            if (singleObj.price_change_percentage_24h < 0) {
+                percentageDiv.style.borderColor = "#e7514a";
+                percentageDiv.style.color = "#e7514a";
+                priceDiv.style.color = "#e7514a";
+            }
 
-                    mainGrid.appendChild(gridElement)
-                })
+            mainGrid.appendChild(gridElement)
+        })
 
-}}
-defaultPageGridData ();
+    }
+}
+defaultPageGridData();
 
 const checkbox = document.getElementById("toggle");
-  const switchElement = document.querySelector(".switch");
+const switchElement = document.querySelector(".switch");
 
-  checkbox.addEventListener("change", function () {
+checkbox.addEventListener("change", function() {
     if (checkbox.checked) {
-      switchElement.style.setProperty("--pseudo-left", "20px");
+        switchElement.style.setProperty("--pseudo-left", "20px");
     } else {
-      switchElement.style.setProperty("--pseudo-left", "0");
+        switchElement.style.setProperty("--pseudo-left", "0");
     }
-  });
+});
 
 
-  const mobilecheckbox = document.getElementById("toggle");
-  const mobileswitchElement = document.querySelector(".mobile-switch");
+const mobilecheckbox = document.getElementById("toggle");
+const mobileswitchElement = document.querySelector(".mobile-switch");
 
-  mobilecheckbox.addEventListener("change", function () {
+mobilecheckbox.addEventListener("change", function() {
     if (mobilecheckbox.checked) {
         mobileswitchElement.style.setProperty("--pseudo-left-", "20px");
     } else {
         mobileswitchElement.style.setProperty("--pseudo-left-", "0");
     }
-  });
-
+});
 
